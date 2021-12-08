5a1970cb571c0bd0f85f08710e6e21138da403fa
Arfandy Adimurfiq <arfandiadimurfiq@gmail.com>
2021-12-08 22:26:54 +0800

    Create a ViewHolder for all the sleep data

--- app/src/main/java/com/example/android/trackmysleepquality/Util.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/Util.kt
@@ -25,31 +25,32 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android.trackmysleepquality.database.SleepNight
 import java.text.SimpleDateFormat
-//import java.util.concurrent.TimeUnit
-//import java.util.*
+
+import java.util.concurrent.TimeUnit
+import java.util.*
 
 
-//private val ONE_MINUTE_MILLIS = TimeUnit.MILLISECONDS.convert(1, TimeUnit.MINUTES)
-//private val ONE_HOUR_MILLIS = TimeUnit.MILLISECONDS.convert(1, TimeUnit.HOURS)
+private val ONE_MINUTE_MILLIS = TimeUnit.MILLISECONDS.convert(1, TimeUnit.MINUTES)
+private val ONE_HOUR_MILLIS = TimeUnit.MILLISECONDS.convert(1, TimeUnit.HOURS)
 
-//fun convertDurationToFormatted(startTimeMilli: Long, endTimeMilli: Long, res: Resources): String {
-//    val durationMilli = endTimeMilli - startTimeMilli
-//    val weekdayString = SimpleDateFormat("EEEE", Locale.getDefault()).format(startTimeMilli)
-//    return when {
-//        durationMilli < ONE_MINUTE_MILLIS -> {
-//            val seconds = TimeUnit.SECONDS.convert(durationMilli, TimeUnit.MILLISECONDS)
-//            res.getString(R.string.seconds_length, seconds, weekdayString)
-//        }
-//        durationMilli < ONE_HOUR_MILLIS -> {
-//            val minutes = TimeUnit.MINUTES.convert(durationMilli, TimeUnit.MILLISECONDS)
-//            res.getString(R.string.minutes_length, minutes, weekdayString)
-//        }
-//        else -> {
-//            val hours = TimeUnit.HOURS.convert(durationMilli, TimeUnit.MILLISECONDS)
-//            res.getString(R.string.hours_length, hours, weekdayString)
-//        }
-//    }
-//}
+fun convertDurationToFormatted(startTimeMilli: Long, endTimeMilli: Long, res: Resources): String {
+    val durationMilli = endTimeMilli - startTimeMilli
+    val weekdayString = SimpleDateFormat("EEEE", Locale.getDefault()).format(startTimeMilli)
+    return when {
+        durationMilli < ONE_MINUTE_MILLIS -> {
+            val seconds = TimeUnit.SECONDS.convert(durationMilli, TimeUnit.MILLISECONDS)
+            res.getString(R.string.seconds_length, seconds, weekdayString)
+        }
+        durationMilli < ONE_HOUR_MILLIS -> {
+            val minutes = TimeUnit.MINUTES.convert(durationMilli, TimeUnit.MILLISECONDS)
+            res.getString(R.string.minutes_length, minutes, weekdayString)
+        }
+        else -> {
+            val hours = TimeUnit.HOURS.convert(durationMilli, TimeUnit.MILLISECONDS)
+            res.getString(R.string.hours_length, hours, weekdayString)
+        }
+    }
+}
 
 
 
@@ -122,7 +123,7 @@ fun formatNights(nights: List<SleepNight>, resources: Resources): Spanned {
                 append("${it.endTimeMilli.minus(it.startTimeMilli) / 1000 / 60}:")
                 // Seconds
                 append("${it.endTimeMilli.minus(it.startTimeMilli) / 1000}<br><br>")
-            }
+//            }
         }
     }
     // fromHtml is deprecated for target API without a flag, but since our minSDK is 19, we
@@ -134,5 +135,5 @@ fun formatNights(nights: List<SleepNight>, resources: Resources): Spanned {
         @Suppress("DEPRECATION")
         return Html.fromHtml(sb.toString())
     }
-}
-class TextItemViewHolder(val textView: TextView): RecyclerView.ViewHolder(textView)
+}}
+class TextItemViewHolder(val textView: TextView): RecyclerView.ViewHolder(textView){}
